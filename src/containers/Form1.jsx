3b94b4d547ffa30dd20303d18e7ea5f39eb790b8
Carter Chavez <Carter.chavez17@gmail.com>
2018-03-31 11:53:56 -0700

    setup form1

--- src/containers/Form1.jsx
+++ src/containers/Form1.jsx
@@ -14,12 +14,70 @@ import { Link } from 'react-router-dom';
 export default class Form1 extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      username: '',
+      password: '',
+      email: '',
+    }
+    
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onEmailChange = this.onEmailChange.bind(this);
   }
+
+  onUsernameChange(event) {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
+  onPasswordChange(event) {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  onEmailChange(event) {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <div>Form1 works</div>
-        <Link to="/form2">Save and Continue to Form 2</Link>
+        <form 
+          className='input-group'
+        >
+          <input
+            placeholder='Enter Username'
+            className='form-control'
+            value={this.state.username}
+            onChange={this.onUsernameChange}
+          />
+
+          <input
+            placeholder='Enter Password'
+            className='form-control'
+            value={this.state.password}
+            onChange={this.onPasswordChange}
+          />
+
+          <input
+            placeholder='Enter Email'
+            className='form-control'
+            value={this.state.email}
+            onChange={this.onEmailChange}
+          />
+
+          <span className="input-group-btn">
+            <Link type='submit' className='btn btn-secondary' to="/form2">
+              Save and Continue to Form 2
+            </Link>
+          </span>     
+        </form>
       </div>
     );
   }
