b754468374c2923295b6d0457f1e190691098e10
Carter Chavez <Carter.chavez17@gmail.com>
2018-04-01 11:10:55 -0700

    update db functions

--- database/index.js
+++ database/index.js
@@ -20,22 +20,29 @@ let Users = Bookshelf.Model.extend({
   tableName: 'users'
 });
 
+let currentUserName = '';
+let countId = 1;
+
 let saveForm1 = (userInfo) => {
   // username password email
+  // create new row with info
+  currentUserName = userInfo.username;
+  new Users({username: currentUserName, password: userInfo.password, email: userInfo.email })
+    .save(null, {method: 'insert'})
+  countId++;
 }
 
 let saveForm2 = (userInfo) => {
   // firstname lastname phonenumber
+  // find username and update
 }
 
 let saveForm3 = (userInfo) => {
   // street city state zip
+  // find username and update
+
 }
 
-
-// module.exports.test = Users.collection().fetch().then( (collection) => {
-//   console.log(collection);
-// });
 module.exports.saveForm1 = saveForm1;
 module.exports.saveForm2 = saveForm2;
 module.exports.saveForm3 = saveForm3;
